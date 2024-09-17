c9e13a135cb760d1237d975bee495f8abe2037d1
xv786vx <firas.aj@icloud.com>
2024-09-17 16:25:39 -0400

    15 3sum + reviewed 53 max subarray and 167 two sum 2

---
+++ leetcode_grind75/15.py
@@ -0,0 +1,29 @@
+# 15. 3Sum
+
+#my solution but really similar to ncs
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        lst = []
+        nums = sorted(nums)
+
+        for i in range(0, len(nums)):
+            if i > 0 and nums[i] == nums[i-1]: # gets rid of duplicates (checking if its == to the one before works cuz the list is sorted)
+                continue
+
+            l, r = i + 1, len(nums) - 1 # for every single i, 2 pointers l and r are used to find the other 2 numbers 
+            while l < r:
+                if nums[i] + nums[l] + nums[r] < 0:
+                    l += 1
+                elif  nums[i] + nums[l] + nums[r] > 0:
+                    r -= 1
+                else:   
+                    lst.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r: # means that the next number is the same as the previous one
+                        l += 1 
+        
+        return lst
--- leetcode_grind75/53.py
+++ leetcode_grind75/53.py
@@ -13,3 +13,20 @@ class Solution:
             maxSub = max(maxSub, cur) #only keeps the max value
         
         return maxSub
+    
+#take 2 from memory
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxsum, cur = nums[0], 0
+        for i in range(0, len(nums)):
+            if cur < 0:
+                cur = 0
+            
+            cur += nums[i]
+            maxsum = max(maxsum, cur)
+        
+        return maxsum
