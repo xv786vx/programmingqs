8b2427f009630b650aa9ab866cc221680807c617
xv786vx <firas.aj@icloud.com>
2024-09-18 12:36:14 -0400

    102 bfs

---
+++ leetcode_grind75/102.py
@@ -0,0 +1,32 @@
+#102. Binary Tree Level Order Traversal
+
+#this is literally just bfs how did i forget this bruh I'm locked out
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        lst = []
+        q = deque()
+        q.append(root)
+
+
+        while q:
+            level = []
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                lst.append(level)
+        return lst
