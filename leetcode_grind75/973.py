58033d3dee9e17a00cb78cb217be089b56b33801
xv786vx <firas.aj@icloud.com>
2024-09-23 20:05:31 -0400

    understand 973 more now

--- leetcode_grind75/973.py
+++ leetcode_grind75/973.py
@@ -1,4 +1,6 @@
 #973. K Closest Points to Origin
+
+#nc ahh solution 
 class Solution(object):
     def kClosest(self, points, k):
         """
@@ -8,10 +10,10 @@ class Solution(object):
         """
         minheap = []
         for x, y in points:
-            dist = (x ** 2) + (y ** 2)
+            dist = (x ** 2) + (y ** 2) 
             minheap.append([dist, x, y])
         
-        heapq.heapify(minheap)
+        heapq.heapify(minheap) #starting from the right-most leaf node, heapify the tree (reference notes for how this happens exactly), pushing the smallest element to the top
         res = []
         while k != 0:
             dist, x, y = heapq.heappop(minheap)
